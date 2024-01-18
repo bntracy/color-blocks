883c97fe385fd9826d8eb7957161ee06fc67e348
Beth Tracy <bntracy@gmail.com>
2024-01-18 14:06:07 -0600

    part one complete

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('🟥 🟦 🟩 🟨');
+
+function createRedBlock() {
+    let blockSection = document.getElementById('blocks');
+    blockSection.innerHTML += '<div class="block red-fill" onclick="deleteBlock(event)"></div>';
+}
+
+function createBlueBlock() {
+    let blockSection = document.getElementById('blocks');
+    blockSection.innerHTML += '<div class="block blue-fill" onclick="deleteBlock(event)"></div>';
+}
+
+function createGreenBlock() {
+    let blockSection = document.getElementById('blocks');
+    blockSection.innerHTML += '<div class="block green-fill" onclick="deleteBlock(event)"></div>';
+}
+
+function createYellowBlock() {
+    let blockSection = document.getElementById('blocks');
+    blockSection.innerHTML += '<div class="block yellow-fill" onclick="deleteBlock(event)"></div>';
+}
+
+function deleteBlock(event) {
+    event.target.remove();
+}
